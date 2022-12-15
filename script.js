a238bc12794db842856aaf726e702b65c8dcef0a
Carlos Ferrer <carlosvferrer@gmail.com>
2022-12-15 18:24:01 -0300

    fatching API working

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
   // Show New Quote
 function newQuote() {
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+  authorText.textContent = quote.author;
+  quoteText.textContent = quote.text;
 }
 
 // Get Quotes from API
 async function getQuotes() {
-  const apiUrl = 'https://type.fit/api/quotes';
+  const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
