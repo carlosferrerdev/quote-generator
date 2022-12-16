825ecbe4f8fbd18547d220d2ee44d0bdabd0b53a
Carlos Ferrer <carlosvferrer@gmail.com>
2022-12-16 16:27:39 -0300

    project with code refactored

--- script.js
+++ script.js
@@ -7,41 +7,37 @@ const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
-// Show Loading
 function loading() {
   loader.hidden = false;
   quoteContainer.hidden = true;
 }
 
-// Hide Loading
 function complete() {
   quoteContainer.hidden = false;
   loader.hidden = true;
 }
 
-// Show New Quote
 function newQuote() {
   loading();
-  // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  //Check if Author field is blank and replace it with 'Unknown'
+
   if (!quote.author) {
     authorText.textContent = 'Unknown'
   } else {
     authorText.textContent = quote.author;
   }
-  // Check quote length to determine styling
+
   if (quote.text.length > 120) {
     quoteText.classList.add('long-quote');
   } else {
     quoteText.classList.remove('long-quote');
   }
-  // Set Quote, Hide Loader
+
   quoteText.textContent = quote.text;
   complete();
 }
 
-// Get Quotes from API
+
 async function getQuotes() {
   loading();
   const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
@@ -50,19 +46,15 @@ async function getQuotes() {
     apiQuotes = await response.json();
     newQuote();
   } catch (error) {
-    // Catch Error Here
   }
 }
 
-// Tweet Quote
 function tweetQuote() {
   const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
   window.open(twitterUrl, '_blank');
 }
 
-// Event Listeners
 newQuoteBtn.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
-//On Load
 getQuotes();
